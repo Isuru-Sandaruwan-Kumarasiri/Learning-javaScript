b284d07dc33d8cb58bffe3ef53c8bf3da26edc35
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-04 08:53:32 +0530

    learning js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-console.log("hello");
-let user=100;
-console.log(user);
+// String
+let str="Hellow world";
+console.log(str)
+
+// Lenght
+let mod_str=str.length;
+console.log(mod_str)
+
+// indexwise
+let mod_str2=str.charAt(0);
+console.log(mod_str2);
+
+// character code
+let mod_str3=str.charCodeAt(0);
+console.log(mod_str3);
+
+//Number rational points
+let num=1000;
+console.log(num.toFixed(2))
+
+//trycatch
+function divider(num1,num2){
+    return num1/num2
+}
+let out =divider(10,0.01);
+console.log(out);
