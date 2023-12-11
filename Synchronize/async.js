a0cfa6bb5c2eee1f8702dde9f366056e93cbe7ba
Isuru-Sandaruwan-Kumarasiri <s20426@sci.pdn.ac.lk>
2023-12-11 06:32:19 +0530

    arrow function

--- Synchronize/async.js
+++ Synchronize/async.js
@@ -1,8 +1,12 @@
 async function getData(){
+   try {
     console.log("Network request start");//T1
     await fetch("https://www.thunderclient.com/welcome");
     console.log("Network request stop");//T3
 
+   } catch (error) {
+    console.log("an error occured")
+   }
 
     
 }
